8576cd9efbea220795793929fc2239e0f154ea5b
itsaladin <alauddin12340@gmail.com>
2022-06-10 01:55:35 +0600

    check time complexcity

--- bigONotation.js
+++ bigONotation.js
@@ -1,9 +1,20 @@
+// case 1 to checking time complex
+// function sumOfAll(n) {
+//   let total = 0;
+//   for (let i = 0; i <= n; i++) {
+//     total += i;
+//   }
+//   return total;
+// }
+
+// case 2 to checking time complex
 function sumOfAll(n) {
-  let total = 0;
-  for (let i = 0; i <= n; i++) {
-    total += i;
-  }
-  return total;
+  return (n * (n + 1)) / 2;
 }
 
-console.log(sumOfAll(10));
+let time1 = performance.now();
+console.log(sumOfAll(5000000000));
+let time2 = performance.now();
+
+let tComplexcity = `get time complexcity = ${(time2 - time1) / 1000}`;
+console.log(tComplexcity);
